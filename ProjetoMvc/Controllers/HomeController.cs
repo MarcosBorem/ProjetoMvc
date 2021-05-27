29e1e6f3959b52f7c44e5deedb3a211ad685badb
Marcos Borem <marcos.borem@hotmail.com>
2021-05-27 10:14:26 -0300

    First controller and Razor pages tests

--- ProjetoMvc/Controllers/HomeController.cs
+++ ProjetoMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace ProjetoMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from c# Course";
+            ViewData["Aluno"] = "Marcos Borém";
 
             return View();
         }
